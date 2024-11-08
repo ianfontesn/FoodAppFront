1b49a6da988a21d07270cd416076ca9a812a6086
Ian Fontes <63882977+ianfontesn@users.noreply.github.com>
2024-11-08 15:03:20 -0300

    refac: Altera o sidenav da esquerda pra direita

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -3,14 +3,23 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-
+import { MatListModule } from '@angular/material/list';
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+  ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
   showFiller: boolean = false;
+
+  items = ['Item 1', 'Item 2', 'Item 3'];
+  expandedIndex = 0;
 }
